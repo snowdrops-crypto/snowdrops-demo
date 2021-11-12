ffdf80aef77531a9cc8548523080fb8d809f5198
VictorS <victor.sh.91@gmail.com>
2021-11-11 23:55:40 -0800

    Updated aweave uploader to work with arlocal and retreive data after upload using transaction id.

--- asset-uploader/index.js
+++ asset-uploader/index.js
@@ -1,22 +1,45 @@
+/* npx arlocal */
+
+const fs = require('fs')
+const path = require('path')
 const Arweave = require('arweave')
 
+// const arweave = Arweave.init({
+//   host: 'arweave.net',
+//   port: 443,
+//   protocol: 'https'
+// })
+
 const arweave = Arweave.init({
-  host: 'arweave.net',
-  port: 443,
-  protocol: 'https'
+  host: '127.0.0.1',
+  port: 1984,
+  protocol: 'http'
 })
 
-(async () => {
-  let transaction = await arweave.createTransaction({ data: data }, keys)
+const run = async () => {
+  const key = await arweave.wallets.generate().then(key => {
+    return key
+  })
+
+  const img = await fs.readFileSync(path.join(__dirname, '../assets/snowdrops-logo-1.png'))
+  console.log(img)
+  let transaction = await arweave.createTransaction({ data: img }, key)
   transaction.addTag('Content-Type', 'png')
-  await arweave.transactions.sign(transaction, keys)
+  await arweave.transactions.sign(transaction, key)
 
   let uploader = await arweave.transactions.getUploader(transaction)
-  console.log(uploader)
 
   while (!uploader.isComplete) {
-    await uploader.uploadChuck()
+    await uploader.uploadChunk()
     console.log(`chunkUploaded: ${uploader.uploadedChunks}/${uploader.totalChunks} %${uploader.pctComplete}`)
-    console.log(` lastResponseStats: ${uploader.lastResponseStatus}`)
+    console.log(`lastResponseStats: ${uploader.lastResponseStatus}`)
   }
-})
+
+  console.log(uploader.transaction.id)
+
+  const img_get = arweave.transactions.get(uploader.transaction.id).then(tnx => {
+    console.log(tnx)
+  })
+}
+
+run()
